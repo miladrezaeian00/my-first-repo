0b6f58914bcf2987120d774934dc5d4cada09c43
miladrezaeian00 <milad.rezaeian00@gmail.com>
2024-09-03 16:08:27 -0400

    create project for compare two file two line two line

---
+++ myscript.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#دریافت فایل اول و دوم از کاربر به همراه متن پیغام و ذخیره آن در متفیر
+read -p "please enter first file: " file1
+read -p "please enter second file: " file2
+
+#بررسی موجود بودن و صحت فایل اول و ارسال پیغام
+if [ -f "$file1" ]; then
+    echo "first file is recieved: $file1"
+else
+    echo "first file not found"
+    exit 1
+fi
+
+#بررسی موجود بودن و صحت فایل دوم و ارسال پیغام
+if [ -f "$file2" ]; then
+    echo "second file is recieved: $file2"
+else
+    echo "second file not found"
+    exit 1
+fi
+
+
+awk 'NR%2{printf "%s ", $0; next} 1' $file1>tempfile1
+awk 'NR%2{printf "%s ", $0; next} 1' $file2>tempfile2
+
+diff -u tempfile1 tempfile2 | grep -E "^\-|\+"
+
